57723552dd90ae1345f9dc4a11174a090fe8b59f
Lazar Ivanov <lazar-ivanov@users.noreply.github.com>
2017-05-07 02:23:53 -0400

    incarnation of http request command in bl-tool

--- src/local/apps/bl-tool/commands/HttpRequest.h
+++ src/local/apps/bl-tool/commands/HttpRequest.h
@@ -28,6 +28,7 @@
 #include <baselib/tasks/ExecutionQueue.h>
 #include <baselib/tasks/Algorithms.h>
 
+#include <baselib/core/FsUtils.h>
 #include <baselib/core/BaseIncludes.h>
 
 namespace bltool
@@ -50,8 +51,9 @@ namespace bltool
 
             BL_CMDLINE_OPTION( m_host,  StringOption,   "host,h",   "The host name",        bl::cmdline::Required )
             BL_CMDLINE_OPTION( m_path,  StringOption,   "path",     "The relative path",    bl::cmdline::Required )
-            BL_CMDLINE_OPTION( m_port,  UShortOption,   "port,p",   "The port (optional)",  80U /* defaultValue */ )
+            BL_CMDLINE_OPTION( m_port,  UShortOption,   "port,p",   "The port (optional)" )
             BL_CMDLINE_OPTION( m_isSsl, BoolSwitch,     "ssl,s",    "Use SSL connection" )
+            BL_CMDLINE_OPTION( m_out,   StringOption,   "out,o",    "Output file to store the response" )
 
             HttpRequestT(
                 SAA_inout    bl::cmdline::CommandBase*          parent,
@@ -65,6 +67,7 @@ namespace bltool
                 addOption( m_path );
                 addOption( m_port );
                 addOption( m_isSsl );
+                addOption( m_out );
 
                 setHelpMessage(
                     "Execute HTTP request and return the result.\n"
@@ -78,7 +81,7 @@ namespace bltool
             <
                 typename IMPL
             >
-            auto executeHttpRequest() -> std::string
+            auto executeHttpRequest( SAA_in const bl::os::port_t port ) -> std::string
             {
                 using namespace bl;
                 using namespace bl::http;
@@ -91,7 +94,7 @@ namespace bltool
                     {
                         const auto taskImpl = IMPL::template createInstance(
                             cpp::copy( m_host.getValue() ),
-                            m_port.getValue(),
+                            port,
                             m_path.getValue(),
                             "GET"               /* action */,
                             ""                  /* content */,
@@ -107,6 +110,23 @@ namespace bltool
                 return response;
             }
 
+            void saveResponse(
+                SAA_in              const std::string&                  response,
+                SAA_inout           std::ostream&                       os
+                )
+            {
+                bl::cpp::SafeInputStringStream is( response );
+
+                std::string line;
+
+                while( is.good() )
+                {
+                    std::getline( is, line );
+
+                    os << line << std::endl;
+                }
+            }
+
             virtual bl::cmdline::Result execute() OVERRIDE
             {
                 using namespace bl;
@@ -115,20 +135,21 @@ namespace bltool
 
                 const bool isSsl = m_isSsl.hasValue() ? true : ( 443U == m_port.getValue() );
 
+                const auto port = m_port.hasValue() ? m_port.getValue() : ( isSsl ? 443U : 80U );
+
                 const auto response = isSsl ?
-                    executeHttpRequest< SimpleHttpSslTaskImpl >()
+                    executeHttpRequest< SimpleHttpSslTaskImpl >( port )
                     :
-                    executeHttpRequest< SimpleHttpTaskImpl >();
+                    executeHttpRequest< SimpleHttpTaskImpl >( port );
 
-                cpp::SafeInputStringStream is( response );
-
-                std::string line;
-
-                while( is.good() )
+                if( m_out.hasValue() )
                 {
-                    std::getline( is, line );
-
-                    std::cout << line;
+                    fs::SafeOutputFileStreamWrapper file( m_out.getValue() );
+                    saveResponse( response, file.stream() );
+                }
+                else
+                {
+                    saveResponse( response, std::cout );
                 }
 
                 return 0;
