21013d7e12e8c246a574b67ad7dde4901018725e
Alex Koren <alexander.koren@jpmchase.com>
2018-01-03 14:34:50 -0500

    jni: test code cleanup

--- java/utf_baselib_jni/src/main/java/org/swblocks/baselib/test/JavaBridgeCallback.java
+++ java/utf_baselib_jni/src/main/java/org/swblocks/baselib/test/JavaBridgeCallback.java
@@ -18,21 +18,21 @@ public class JavaBridgeCallback {
     private void syncCallbackTest(final ByteBuffer inBuffer, final ByteBuffer outBuffer, final long callbackId) {
         final String inString = JavaBridgeCommon.readString(inBuffer);
 
-        String[] words = inString.toUpperCase().split(" ");
+        final String[] words = inString.toUpperCase().split(" ");
 
-        byte[] inArray = new byte[100];
-        ByteBuffer wrappedInBuffer = ByteBuffer.wrap(inArray);
+        final byte[] inArray = new byte[100];
+        final ByteBuffer wrappedInBuffer = ByteBuffer.wrap(inArray);
         wrappedInBuffer.order(ByteOrder.nativeOrder());
 
-        byte[] outArray = new byte[100];
-        ByteBuffer wrappedOutBuffer = ByteBuffer.wrap(outArray);
+        final byte[] outArray = new byte[100];
+        final ByteBuffer wrappedOutBuffer = ByteBuffer.wrap(outArray);
         wrappedOutBuffer.order(ByteOrder.nativeOrder());
 
         ByteBuffer localInBuffer;
         ByteBuffer localOutBuffer;
 
         int counter = 0;
-        for (String word : words) {
+        for (final String word : words) {
             if (counter++ % 2 == 0) {
                 localInBuffer = inBuffer;
                 localOutBuffer = outBuffer;
@@ -45,7 +45,7 @@ public class JavaBridgeCallback {
             JavaBridgeCommon.writeString(localInBuffer, word);
             nativeCallback(localInBuffer, localOutBuffer, callbackId);
 
-            String word2 = JavaBridgeCommon.readString(localOutBuffer);
+            final String word2 = JavaBridgeCommon.readString(localOutBuffer);
             if (!word2.equals(word + word)) {
                 throw new RuntimeException("Unexpected word in the callback direct output buffer: " + word2);
             }
