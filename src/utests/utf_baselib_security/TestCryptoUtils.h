e5493f47fc8a122eb22c7db78b913641e05318c9
Steven Durosier <steven.durosier@jpmorgan.com>
2018-04-11 11:59:19 -0400

    Add decryption methods

--- src/utests/utf_baselib_security/TestCryptoUtils.h
+++ src/utests/utf_baselib_security/TestCryptoUtils.h
@@ -18,10 +18,41 @@
 #include <baselib/crypto/CryptoBase.h>
 #include <baselib/crypto/X509Cert.h>
 
+#include <baselib/security/JsonSecuritySerializationImpl.h>
+#include <baselib/data/models/Jose.h>
+#include <baselib/crypto/RsaEncryption.h>
 #include <baselib/core/BaseIncludes.h>
 
 #include <utests/baselib/Utf.h>
 #include <utests/baselib/UtfArgsParser.h>
+
+class JoseTypesPolicy
+{
+    BL_DECLARE_STATIC( JoseTypesPolicy )
+
+public:
+
+    typedef bl::dm::jose::RsaPublicKey           RsaPublicKey;
+    typedef bl::dm::jose::RsaPrivateKey          RsaPrivateKey;
+    typedef bl::dm::jose::KeyType                KeyType;
+    typedef bl::dm::jose::SigningAlgorithm       SigningAlgorithm;
+    typedef bl::dm::jose::PublicKeyUse           PublicKeyUse;
+
+};
+
+bl::om::ObjPtr< bl::crypto::RsaKey > getRsaKeyFromFile( SAA_in const std::string& fileName )
+{
+	using namespace  bl::security;
+
+	const auto rsaKeyStr = utest::TestUtils::loadDataFile( fileName );
+
+    if( fileName.find( "private" ) != std::string::npos )
+    {
+    	return JsonSecuritySerializationImpl< JoseTypesPolicy >::loadPrivateKeyFromPemString( rsaKeyStr );
+    }
+
+    return JsonSecuritySerializationImpl< JoseTypesPolicy >::loadPublicKeyFromPemString( rsaKeyStr );
+}
 
 UTF_AUTO_TEST_CASE( CryptoUtils_InitSsl )
 {
@@ -57,4 +88,71 @@ UTF_AUTO_TEST_CASE( CryptoUtils_X509tests )
     const auto x509certPem = bl::crypto::geX509CertAsPemString( x509cert );
 
     UTF_REQUIRE( x509certPem.find( "-----BEGIN CERTIFICATE-----" ) != std::string::npos );
+}
+
+UTF_AUTO_TEST_CASE( RsaEncryption_encryptAsBase64Tests )
+{
+	const std::string password = "mypass123";
+
+    const auto publicKey = getRsaKeyFromFile( "test-public-key.pem" );
+
+    const auto encryptedPass = bl::crypto::RsaEncryption::encryptAsBase64Url( publicKey, password );
+
+    const auto privateKey =  getRsaKeyFromFile( "test-private-key.pem" );
+
+    const auto decrypted = bl::crypto::RsaEncryption::decryptBase64Message( privateKey, encryptedPass );
+
+    UTF_CHECK( decrypted == password );
+
+    UTF_CHECK( decrypted != encryptedPass );
+}
+
+UTF_AUTO_TEST_CASE( RsaEncryption_encryptTests )
+{
+	const std::string password = "mypass123";
+
+    const auto publicKey = getRsaKeyFromFile( "test-public-key.pem" );
+
+    unsigned outputSize = 0u;
+
+    const auto out = bl::crypto::RsaEncryption::encrypt( publicKey, password, outputSize );
+
+    const auto charBuffer = ( unsigned char * ) out.get();
+
+    const std::string encryptedPassword( reinterpret_cast< char* >( charBuffer ) );
+
+    UTF_CHECK( ! encryptedPassword.empty() );
+
+    UTF_CHECK( encryptedPassword != password );
+}
+
+UTF_AUTO_TEST_CASE( RsaEncryption_decryptTests )
+{
+	const std::string password = "mypass123";
+
+    const auto publicKey = getRsaKeyFromFile( "test-public-key.pem" );
+
+    unsigned outputSize = 0u;
+    const auto out = bl::crypto::RsaEncryption::encrypt( publicKey, password, outputSize );
+
+    const auto encryptedBuffer = ( unsigned char * ) out.get();
+
+    const std::string encryptedPassword( reinterpret_cast< char* >( encryptedBuffer ) );
+
+    UTF_CHECK( ! encryptedPassword.empty() );
+
+    UTF_CHECK( encryptedPassword != password );
+
+
+    const auto privateKey = getRsaKeyFromFile( "test-private-key.pem" );
+
+    const auto out2 = bl::crypto::RsaEncryption::decrypt( privateKey, encryptedPassword, outputSize );
+
+	const auto decryptedBuffer = ( unsigned char * ) out2.get();
+
+	const std::string decryptedPassword( reinterpret_cast< char* >( decryptedBuffer ) );
+
+	UTF_CHECK( ! decryptedPassword.empty() );
+
+	UTF_CHECK_EQUAL( decryptedPassword, password );
 }
